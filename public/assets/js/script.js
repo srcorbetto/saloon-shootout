1dd5aeca85f160355f1ce995b27523efd41e1bb6
Mike Corbett <srcorbetto@gmail.com>
2019-11-07 12:52:49 -0600

    added texture and updated animation

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,20 +1,22 @@
 const gun = document.getElementById('gun');
+let recoil = false;
 
 document.getElementById('box').addEventListener('mouseenter', e => {
     gun.setAttribute('animation__recoil', {
         property: 'rotation',
         to: '-11 183 0',
-        dur: 25
+        dur: 50
     });
     gun.setAttribute('animation__reset', {
         property: 'rotation',
         to: '0 183 0',
         dur: 75,
-        delay: 45
+        delay: 75
     });
-    console.log(gun.attributes);
-    // gun.removeAttribute('animation__recoil');
-    // gun.removeAttribute('animation__reset');
+    setTimeout(() => {
+        gun.removeAttribute('animation__recoil');
+        gun.removeAttribute('animation__reset');
+    }, 100)
 });
 
 // document.getElementById('my-camera')
