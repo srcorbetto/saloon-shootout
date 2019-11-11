5609604e1b202762055deae83bd634eecee3782d
Mike Corbett <srcorbetto@gmail.com>
2019-11-11 13:00:04 -0600

    Balloon actions

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,38 @@
 const gun = document.getElementById('gun');
 const flash = document.getElementById('flash');
-const balloonIds = ['balloonA', 'balloonB', 'balloonC', 'balloonD'];
-
 const classname = document.getElementsByClassName('balloon');
+const sceneElement = document.querySelector('a-scene');
 
-const shootBalloon = e => {
-    console.log(e.target);
+const createBalloon = () => {
+    const balloonElement = document.createElement('a-gltf-model');
+    const balloonAnimations = [
+        {
+            id: 'balloonA',
+            class: 'balloon',
+            position: '0 3 0.5',
+            scale: '3 3 3',
+            animation: 'property: position; to: 0 4 0.5; loop: true; dur: 3000; dir: alternate'
+        },
+        {
+            id: 'balloonB',
+            class: 'balloon',
+            position: '-1.7 3 0.5',
+            scale: '3 3 3',
+            animation: 'property: position; to: -1.7 4 0.5; loop: true; dur: 3000; dir: alternate'
+        }
+    ];
+    console.log(balloonAnimations[0].position);
+    sceneElement.appendChild(balloonElement);
+    balloonElement.setAttribute('id', balloonAnimations[0].id);
+    balloonElement.setAttribute('class', balloonAnimations[0].class);
+    balloonElement.setAttribute('position', balloonAnimations[0].position);
+    balloonElement.setAttribute('scale', balloonAnimations[0].scale);
+    balloonElement.setAttribute('animation', balloonAnimations[0].animation);
+    balloonElement.addEventListener('mouseenter', shootBalloon, false);
+    console.log(balloonElement);
+}
+
+const gunShot = () => {
     gun.setAttribute('animation__recoil', {
         property: 'rotation',
         to: '-11 183 0',
@@ -25,31 +52,16 @@ const shootBalloon = e => {
         gun.removeAttribute('animation__recoil');
         gun.removeAttribute('animation__reset');
     }, 400);
-};
-
-for (let i = 0; i < classname.length; i++) {
-    classname[i].addEventListener('mouseenter', shootBalloon, false);
 }
 
-// document.getElementById('balloon').addEventListener('mouseenter', e => {
-//     console.log('e',e.target.id);
-//     gun.setAttribute('animation__recoil', {
-//         property: 'rotation',
-//         to: '-11 183 0',
-//         dur: 50
-//     });
-//     gun.setAttribute('animation__reset', {
-//         property: 'rotation',
-//         to: '0 183 0',
-//         dur: 200,
-//         delay: 100
-//     });
-//     flash.setAttribute('opacity', 1);
-//     setTimeout(() => {
-//         flash.setAttribute('opacity', 0);
-//     }, 150);
-//     setTimeout(() => {
-//         gun.removeAttribute('animation__recoil');
-//         gun.removeAttribute('animation__reset');
-//     }, 400);
-// });
+const shootBalloon = e => {
+    const targetBaloon = document.getElementById(e.target.id);
+    gunShot();
+    targetBaloon.parentNode.removeChild(targetBaloon);
+};
+
+createBalloon();
+
+// for (let i = 0; i < classname.length; i++) {
+//     classname[i].addEventListener('mouseenter', shootBalloon, false);
+// }
