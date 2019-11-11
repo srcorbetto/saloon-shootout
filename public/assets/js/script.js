d0114083f3deaa54c3a8cf2824b1ab4c3753a377
Mike Corbett <srcorbetto@gmail.com>
2019-11-10 20:31:51 -0600

    Multiple balloons with eventlisteners

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,11 @@
 const gun = document.getElementById('gun');
 const flash = document.getElementById('flash');
+const balloonIds = ['balloonA', 'balloonB', 'balloonC', 'balloonD'];
 
-document.getElementById('balloon').addEventListener('mouseenter', e => {
+const classname = document.getElementsByClassName('balloon');
+
+const shootBalloon = e => {
+    console.log(e.target);
     gun.setAttribute('animation__recoil', {
         property: 'rotation',
         to: '-11 183 0',
@@ -21,9 +25,31 @@ document.getElementById('balloon').addEventListener('mouseenter', e => {
         gun.removeAttribute('animation__recoil');
         gun.removeAttribute('animation__reset');
     }, 400);
-});
+};
 
-// document.getElementById('my-camera')
-//   .addEventListener('rotationChanged', e => {
-//     console.log('New rotation:', e.detail);
-//   });
+for (let i = 0; i < classname.length; i++) {
+    classname[i].addEventListener('mouseenter', shootBalloon, false);
+}
+
+// document.getElementById('balloon').addEventListener('mouseenter', e => {
+//     console.log('e',e.target.id);
+//     gun.setAttribute('animation__recoil', {
+//         property: 'rotation',
+//         to: '-11 183 0',
+//         dur: 50
+//     });
+//     gun.setAttribute('animation__reset', {
+//         property: 'rotation',
+//         to: '0 183 0',
+//         dur: 200,
+//         delay: 100
+//     });
+//     flash.setAttribute('opacity', 1);
+//     setTimeout(() => {
+//         flash.setAttribute('opacity', 0);
+//     }, 150);
+//     setTimeout(() => {
+//         gun.removeAttribute('animation__recoil');
+//         gun.removeAttribute('animation__reset');
+//     }, 400);
+// });
