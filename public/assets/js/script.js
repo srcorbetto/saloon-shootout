8d5fc63eed693d17bb56c725bb1881f5d618eb41
Mike Corbett <srcorbetto@gmail.com>
2019-11-13 18:57:24 -0600

    Balloons showing in random locations

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,8 @@
 const gun = document.getElementById('gun');
 const flash = document.getElementById('flash');
 const classname = document.getElementsByClassName('balloon');
-const sceneElement = document.querySelector('a-scene');
 
 const createBalloon = () => {
-    const balloonElement = document.createElement('a-gltf-model');
     const balloonAnimations = [
         {
             id: 'balloonA',
@@ -21,19 +19,38 @@ const createBalloon = () => {
             scale: '3 3 3',
             animation: 'property: position; to: -1.7 4 0.5; loop: true; dur: 3000; dir: alternate',
             src: '#balloonGltf'
+        },
+        {
+            id: 'balloonC',
+            class: 'balloon',
+            position: '-3 3 0.5',
+            scale: '3 3 3',
+            animation: 'property: position; to: -3 4 0.5; loop: true; dur: 3000; dir: alternate',
+            src: '#balloonGltf'
+        },
+        {
+            id: 'balloonD',
+            class: 'balloon',
+            position: '5 3 0.5',
+            scale: '3 3 3',
+            animation: 'property: position; to: 5 4 0.5; loop: true; dur: 3000; dir: alternate',
+            src: '#balloonGltf'
         }
     ];
-    console.log(balloonAnimations[0]);
+    const sceneElement = document.querySelector('a-scene');
+    const balloonElement = document.createElement('a-gltf-model');
+    const arrayLenth = balloonAnimations.length - 1;
+    const randomNumber = Math.floor(Math.random() * (arrayLenth - 0 + 1)) + 0; //The maximum is inclusive and the minimum is inclusive
+
     sceneElement.appendChild(balloonElement);
     balloonElement.addEventListener('loaded', e => {
-        balloonElement.setAttribute('id', balloonAnimations[0].id);
-        balloonElement.setAttribute('class', balloonAnimations[0].class);
-        balloonElement.setAttribute('position', balloonAnimations[0].position);
-        balloonElement.setAttribute('src', balloonAnimations[0].src);
-        balloonElement.setAttribute('scale', balloonAnimations[0].scale);
-        balloonElement.setAttribute('animation', balloonAnimations[0].animation);
+        balloonElement.setAttribute('id', balloonAnimations[randomNumber].id);
+        balloonElement.setAttribute('class', balloonAnimations[randomNumber].class);
+        balloonElement.setAttribute('position', balloonAnimations[randomNumber].position);
+        balloonElement.setAttribute('src', balloonAnimations[randomNumber].src);
+        balloonElement.setAttribute('scale', balloonAnimations[randomNumber].scale);
+        balloonElement.setAttribute('animation', balloonAnimations[randomNumber].animation);
         balloonElement.addEventListener('mouseenter', shootBalloon, false);
-        console.log(e);
     })
 }
 
@@ -63,10 +80,12 @@ const shootBalloon = e => {
     const targetBaloon = document.getElementById(e.target.id);
     gunShot();
     targetBaloon.parentNode.removeChild(targetBaloon);
+    setTimeout(() => {
+        createBalloon();
+    }, 1000);
 };
 
 createBalloon();
-
 // for (let i = 0; i < classname.length; i++) {
 //     classname[i].addEventListener('mouseenter', shootBalloon, false);
 // }
